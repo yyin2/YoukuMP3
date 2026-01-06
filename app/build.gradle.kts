442cd57ac108f00404ecd69c6eca75dac9d9506d
yinyan <yinyan192@gmail.com>
2026-01-06 17:59:35 +0800

    fix yt-dlp init error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,12 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        ndk {
+            abiFilters.add("x86")
+            abiFilters.add("x86_64")
+            abiFilters.add("armeabi-v7a")
+            abiFilters.add("arm64-v8a")
+        }
     }
 
     buildTypes {
